fb8869aa70bc04ea319e08df6979c826bd572ba6
Bethany Raymond <bethanyraymond66@gmail.com>
2024-01-25 21:17:43 -0500

    Added persistence code from Chapter 5

---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,41 @@
+package com.polarbookshop.catalogservice.domain;
+
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.polarbookshop.catalogservice.config.DataConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = repository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
